3871b5fd688ce2b610b3c5b0684462890edfc14f
lexzino <alexudeze001@gmail.com>
2024-06-25 09:04:52 +0000

    task completed

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const returnArray = [];
+    for (const value of array) {
+      returnArray.push(appendString + value);
+    }
+  
+    return returnArray;
+  } 
