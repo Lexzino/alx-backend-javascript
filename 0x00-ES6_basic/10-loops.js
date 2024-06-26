1bc2fb8fa0b14af91ae4e1edf22208fba3f546e2
lexzino <alexudeze001@gmail.com>
2024-06-26 10:48:28 +0000

    Passes lint corrected

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const returnArray = [];
-    for (const value of array) {
-      returnArray.push(appendString + value);
-    }
-  
-    return returnArray;
-  } 
+  const returnArray = [];
+  for (const value of array) {
+    returnArray.push(appendString + value);
+  }
+
+  return returnArray;
+}
